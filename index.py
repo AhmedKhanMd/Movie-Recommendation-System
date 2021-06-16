3071fb3079ebc5cf336ead3837639885421d53e2
AhmedKhanMd <ahmedkhan_md18@vnrvjiet.in>
2021-06-16 18:35:17 +0530

    Getting related movies from movie-titles"

--- index.py
+++ index.py
@@ -20,6 +20,23 @@ def extract_movie_titles(resultsFromTastedive):
         
     return movieTitles
 
-print(extract_movie_titles(get_movies_from_tastedive("Black Panther")))
+def get_related_titles(movieTitles):
     
+    resultingList = []
+    
+    for movieTitle in movieTitles:
+        
+        relatedMoviesData = get_movies_from_tastedive(movieTitle)
+        
+        relatedMovies = extract_movie_titles(relatedMoviesData)
+        
+        for i in relatedMovies:
+            
+            if i not in resultingList:
+                
+                resultingList.append(i)
+                
+    return resultingList
 
+print(get_related_titles(["Black Panther", "Captain Marvel"]))
+    
