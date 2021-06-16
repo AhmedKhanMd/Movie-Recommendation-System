3d97267aba21f1c8966cc4b16f020bf31772b3e3
AhmedKhanMd <ahmedkhan_md18@vnrvjiet.in>
2021-06-16 19:31:10 +0530

    Fetching rotten tomatoes rating for given movie

--- index.py
+++ index.py
@@ -46,6 +46,15 @@ def get_movie_data(movieTitle):
     
     return results.json()
 
+def get_movie_rating(movieInfo):
+    
+    for i in movieInfo['Ratings']:
+        
+        if i['Source'] == 'Rotten Tomatoes':
+       
+            return int(i['Value'][:2])
+
+    return 0
 
 # print(get_related_titles(["Black Panther", "Captain Marvel"]))
 # print(get_movie_data("Venom"))
